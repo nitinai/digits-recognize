28791623f64c151c1329d68a59cff98a3ded2f3f
Patil, Nitin (PrdEng) <nitin_patil@siemens.com>
2019-08-05 02:59:30 +0530

    torch

--- app.py
+++ app.py
@@ -8,16 +8,10 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from models.cnn import *
 
-#import logging
-
 app = Flask(__name__)
 CORS(app, headers=['Content-Type'])
-#logging.getLogger('flask_cors').level = logging.DEBUG
-
 
 model = CNN()
-if not os.path.exists('models/model.pkl'):
-	print("Model file does not exists at models/model.pkl")
 model.load_state_dict(torch.load("models/model.pkl", map_location='cpu'))
 model.eval()
 
@@ -28,39 +22,22 @@ def index_page():
 
 @app.route('/classify', methods = ["POST"])
 def predict():
-	"""
-	Decodes image and uses it to make prediction.
-	"""
-	with open("log.txt", 'w') as f:
-		#f.write("Entered hook2 predict")
-
-		#f.write(f"request.method {request.method}\n")
-		
-		if request.method == 'POST':
-			image_b64 = request.values['imageBase64']
-			image_encoded = image_b64.split(',')[1]
-			image = base64.decodebytes(image_encoded.encode('utf-8'))
-			#f.write(f"image {image}\n")
-			prediction = single_predict(model,image)
+	"Decodes image and uses it to make prediction"
+	if request.method == 'POST':
+		image_b64 = request.values['imageBase64']
+		image_encoded = image_b64.split(',')[1]
+		image = base64.decodebytes(image_encoded.encode('utf-8'))
+		prediction = single_predict(model,image)
 
 	return json.dumps(prediction)
 
 
 @app.route('/randomNum', methods = ["GET"])
 def generateRandomNumber():
-	"""
-	generateRandomNumber
-	"""
-	with open("log.txt", 'w') as f:
-		f.write("Entered randomNum generateRandomNumber")
-		f.write(f"request.method {request.method}")
-		
 	prediction = random.randint(0, 100)
-
 	return json.dumps(prediction)
 		
 
 if __name__ == '__main__':
-	
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port, debug=False)
--- models/cnn.py
+++ models/cnn.py
@@ -1,7 +1,6 @@
 __author__ = "Nitin Patil"
 
 import os
-import uuid
 import numpy as np
 from PIL import Image
 
@@ -10,10 +9,8 @@ import torch.nn.functional as F
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 
-SEED = 42
-
 class MNIST_Dataset(Dataset):
-    """Custom Dataset for loading MNIST images"""
+    "Custom Dataset for loading MNIST images"
 
     def __init__(self, img, transform=None):
         self.X = img.reshape((-1,28,28)).astype(np.uint8)[:,:,:,None]
@@ -57,7 +54,6 @@ class CNN(torch.nn.Module):
                       kernel_size=5,
                       stride=1,
                       padding=2),
-            #torch.nn.BatchNorm2d(32),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2,
                          stride=2,
@@ -70,7 +66,6 @@ class CNN(torch.nn.Module):
                       kernel_size=5,
                       stride=1,
                       padding=1),
-            #torch.nn.BatchNorm2d(64),
             torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size=2,
                          stride=2,
@@ -87,7 +82,6 @@ class CNN(torch.nn.Module):
         out = self.layer2(out)
         out = self.bn2(out)
         out = out.reshape(out.size(0), -1)
-        #out = self.drop_out(out)
         out = self.linear_1(out)
         out = F.relu(out)
         logits = self.linear_2(out.view(-1, 1000))
@@ -95,41 +89,26 @@ class CNN(torch.nn.Module):
         return logits, probas
     
     
-#################################
-### Model Initialization
-#################################
-
 def preprocess_image(image):
     """
         Save and open an image and resize to 28x28
         Return numpy array
     """
-    filename = 'digit' +  '__' + str(uuid.uuid1()) + '.jpg'
-    with open('tmp/' + filename, 'wb') as f:
-        f.write(image)
-        
     # open and convert the image to grayscale
-    img = Image.open('tmp/' + filename).convert(mode='L')
+    img = image.convert(mode='L')
     
     # create a thumbnail and preserve aspect ratio
     img.thumbnail((28,28))
     return np.asarray(img)
 
 def prediction(model, data_loader):
-    test_pred = torch.LongTensor()#.to(DEVICE)
-    with open("log.txt", 'w') as f:
-        f.write("prediction\n")
-        for features in data_loader:
-            #features = features#.to(DEVICE)
-            logits, probas = model(features)
-            _, predicted_labels = torch.max(probas,1)
-            f.write(f"logits : {logits}\n")
-            f.write(f"probas : {probas}\n")
-            f.write(f"predicted_labels : {predicted_labels}\n")
-            test_pred = torch.cat((test_pred, predicted_labels), dim=0)
+    test_pred = torch.LongTensor()
+    
+    for features in data_loader:
+        logits, probas = model(features)
+        _, predicted_labels = torch.max(probas,1)
+        test_pred = torch.cat((test_pred, predicted_labels), dim=0)
 
-        f.write(f"test_pred : {test_pred}\n")
-            
     return test_pred
 
 def single_predict(model, image):
@@ -137,28 +116,10 @@ def single_predict(model, image):
     if image is None:
         return "Can't predict, when nothing is drawn"
 
-    batch_size = 1
-
-    with open("log.txt", 'w') as f:
-        f.write("Entered single_predict\n")
-    
-        f.write(f"Image type : {type(image)}\n")
-        
-        test_dataset = MNIST_Dataset(image,
-                                    transform=test_transform)
-
-        f.write(f"test_dataset : {type(test_dataset)}\n")
-        
-        test_loader = DataLoader(dataset=test_dataset,
-                                 batch_size=batch_size,
-                                 shuffle=False)
-                                 
-        f.write(f"test_loader : {type(test_loader)}\n")
-        
-        test_pred = prediction(model, test_loader)
-        
-        f.write(f"test_pred : {test_pred}\n")
-        f.write(f"test_pred Shape : {test_pred.shape}\n")
-        
-        return test_pred.item()
-
+    test_dataset = MNIST_Dataset(image, transform=test_transform)
+    test_loader = DataLoader(dataset=test_dataset,
+                                batch_size=1,
+                                shuffle=False)
+                                
+    test_pred = prediction(model, test_loader)
+    return test_pred.item()
