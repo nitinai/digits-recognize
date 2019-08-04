6555b4abbaab1cdbe42d01bff8a0b3ee21753f21
Patil, Nitin (PrdEng) <nitin_patil@siemens.com>
2019-08-05 02:39:52 +0530

    empty

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def generateRandomNumber():
 if __name__ == '__main__':
 	
 	port = int(os.environ.get("PORT", 5000))
-	app.run( port=port, debug=True)
+	app.run(host='0.0.0.0', port=port, debug=False)
